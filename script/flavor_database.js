4677f8aa4bc452854a880ee4ff5c6799243cf8bf
jigar012 <vaishnavjigar86@gmail.com>
2026-02-20 12:44:27 +0530

    Add files via upload

---
+++ script/flavor_database.js
@@ -0,0 +1,24 @@
+const falvors_list = [
+    {
+        id: "f001",
+        name: "Regular"
+    },
+    {
+        id: "f002",
+        name: "Gelato"
+    },
+    {
+        id: "f003",
+        name: "Sorbet"
+    },
+    {
+        id: "f004",
+        name: "Frozen Yogurt"
+    },
+    {
+        id: "f005",
+        name: "Non-Dairy/Vegan"
+    }
+]
+
+export default falvors_list;
